e4bdd93e9c0748699a23347336a56b03107ad119
Anh Do <do.dinh.anh@linecorp.com>
2023-03-07 22:52:59 +0700

    Do fade in and fade out effects on images

--- src/components/Image/index.tsx
+++ src/components/Image/index.tsx
@@ -1,11 +1,21 @@
+import classNames from 'classnames'
 import type { ImageProps } from 'next/image'
+import { useState } from 'react'
 import NextImage from './NextImage'
+
+import styles from './index.module.scss'
 
 type Props = ImageProps & {
   fallbackSrc?: string
 }
 
 const Image: React.FC<Props> = (props) => {
+  const [isLoaded, setIsLoaded] = useState(false)
+
+  function handleLoad() {
+    setIsLoaded(true)
+  }
+
   const handleError = (e: React.SyntheticEvent<HTMLImageElement>) => {
     const target = e.target as HTMLImageElement
     target.removeAttribute('srcset')
@@ -15,9 +25,20 @@ const Image: React.FC<Props> = (props) => {
     } else {
       target.style.display = 'none'
     }
+    setIsLoaded(true)
   }
 
-  return <NextImage {...props} onError={handleError} />
+  return (
+    <NextImage
+      {...props}
+      className={classNames(
+        props.className,
+        isLoaded ? styles.fadeIn : styles.fadeOut
+      )}
+      onLoad={handleLoad}
+      onError={handleError}
+    />
+  )
 }
 
 export default Image
